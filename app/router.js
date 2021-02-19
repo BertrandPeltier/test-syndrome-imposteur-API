b1d98a7235fe45ef26b9a27f134155e630ee49cc
BertrandPeltier <bertrandpeltier1979@gmail.com>
2021-02-20 00:40:51 +0100

    Controller OK + Tests insomnia OK

--- app/router.js
+++ app/router.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
+const mainController = require('./controllers/mainController');
+
 const router = express.Router();
+
 
 router.get('/', (req, res) => {
     res.send('API test du syndrome de l\'imposteur');
 });
+
+router.get('/questions', mainController.getQuestions);
+
+router.get('/answers', mainController.getAnswers);
 
 module.exports = router;
